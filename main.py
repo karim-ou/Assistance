d177c7d723bc3626d4edbd650aaeb8bd0238589b
karim <karimoumansour1@gmail.com>
2021-01-11 20:38:51 +0100

    add comments and make the program easy to read

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+""" Alexa an Assistance do simple tasks"""
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
@@ -7,46 +8,60 @@ import pyjokes
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
+# Give Alexa a female voice
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
 
 def talk(text):
+    """
+create talk function to give Alexa the ability to say whatever the text is
+    :param text: depend on the command
+    """
     engine.say(text)
     engine.runAndWait()
 
 
 def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
-    except:
-        pass
+    """
+take the command from the user, and the command will be printed is alexa's in the command
+    :return: the command
+    """
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            command = command.replace('alexa', '')
+            print(command)
+
     return command
 
 
 def run_alexa():
+    """
+search i the command for keywords
+    """
     command = take_command()
     print(command)
+    # if play was in the command then program will play what's after play on youtube.
     if 'play' in command:
         song = command.replace('play', '')
         talk('playing' + song)
         pywhatkit.playonyt(song)
+    # if time was in the command then  the program tell you what's the current time.
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%H %p')
         print(time)
         talk('Current time is ' + time)
+    # if search for was in the command then the program will search in wikipedia for what's after search for.
     elif 'search for' in command:
         something = command.replace('search for', '')
         info = wikipedia.summary(something, 2)
         print(info)
         talk(info)
+    # if joke was in the command then the program will tell you a joke
     elif 'joke' in command:
         talk(pyjokes.get_joke())
     else:
@@ -54,4 +69,5 @@ def run_alexa():
 
 
 while True:
+    # the program will run till you don't give a command or the program didn't understand the command
     run_alexa()
